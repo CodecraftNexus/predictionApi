e2f33892d54df27093499609dddd4ba5f6cdd17a
Damith Akalanka <105867288+CodecraftNexus@users.noreply.github.com>
2025-12-21 19:07:53 +0530

    main

--- dist/routes/ApikeyRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const apiKeyController_1 = require("../controllers/apiKeyController");
-const router = (0, express_1.Router)();
-router.post("/Apikey", apiKeyController_1.ApiKeyPost);
-router.get("/Apikey", apiKeyController_1.getApiKey);
-router.put("/Apikey", apiKeyController_1.putApikey);
-exports.default = router;
